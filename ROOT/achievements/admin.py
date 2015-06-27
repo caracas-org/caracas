0823850c24e6f634ae9645591e7d2350a385c6c5
sisch <sisch105@hhu.de>
2015-06-27 17:04:36 +0200

    Unlock achievement API call implemented

--- ROOT/achievements/admin.py
+++ ROOT/achievements/admin.py
@@ -1,7 +1,8 @@
-from .models import Achievement, APIUser
+from .models import Achievement, APIUser, AchievementUnlocked
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(Achievement)
+admin.site.register(AchievementUnlocked)
 admin.site.register(APIUser)
--- ROOT/achievements/views.py
+++ ROOT/achievements/views.py
@@ -1,10 +1,12 @@
-from .models import Achievement, APIUser
+from django.utils import timezone
+from .models import Achievement, APIUser, AchievementUnlocked
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 
 class ProgressSerializer(serializers.Serializer):
     achievement_id = serializers.CharField()
@@ -83,7 +85,28 @@ class UnlockProgress(APIView):
     def post(self, request):
         serializer = ProgressSerializer(data=request.data)
         if serializer.is_valid():
-            return Response({'progress': 'test'})
+            response = dict()
+            achievement = Achievement.objects.get(id=serializer.validated_data['achievement_id'])
+            achievement_unlocked = AchievementUnlocked.objects.get(achievement_id=serializer.validated_data['achievement_id'], character__user_id=serializer.validated_data['user_id'])
+            #get or create
+            response['fulfilled'] = (achievement.max_progress == achievement_unlocked.progress)
+            response['achievement_id'] = achievement.id
+            response['achievement_image'] = achievement.icon.url
+            response['achievement_name'] = achievement.name
+            response['achievement_max_progress'] = achievement.max_progress
+            response['progress'] = min(serializer.validated_data['progress'], achievement.max_progress)
+            response['xp_gained'] = 0 if not response['fulfilled'] else achievement.XP_gained
+            if achievement_unlocked.progress > serializer.validated_data['progress']:
+                raise ValueError("Progress must be larger than current progress")
+            if not response['fulfilled']:
+                if response['progress'] > achievement_unlocked.progress:
+                    achievement_unlocked.progress = response['progress']
+                    if response['progress'] >= achievement.max_progress:
+                        response['fulfilled'] = True
+                        achievement_unlocked.fulfilled = True
+                        response['xp_gained'] = achievement.XP_gained
+                    achievement_unlocked.save()
+            return Response(response)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
